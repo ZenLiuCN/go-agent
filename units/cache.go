92fefd7abc723a166b9025e09ac55876cdecd9ed
Zen.Liu <lcz20@163.com>
2024-04-28 15:37:31 +0800

    fix

--- units/cache.go
+++ units/cache.go
@@ -1,6 +1,7 @@
 package units
 
 import (
+	"github.com/ZenLiuCN/gofra/units"
 	"time"
 )
 
@@ -12,16 +13,16 @@ func (c Cached[K, V]) Close() error {
 	c.Cache.StopKeeping()
 	return nil
 }
-func NewStringCache(freq, ttl time.Duration, units units.MeasureUnit, opt ...units.Option) (c *Cached[string, any]) {
+func NewStringCache(freq, ttl time.Duration, u units.MeasureUnit, opt ...units.Option) (c *Cached[string, any]) {
 	c = &Cached[string, any]{
-		Cache: units.NewCache[string, any]("", nil, freq, ttl, units, opt...),
+		Cache: units.NewCache[string, any]("", nil, freq, ttl, u, opt...),
 	}
 	c.StartKeeping()
 	return
 }
-func NewNumberCache(freq, ttl time.Duration, units units.MeasureUnit, opt ...units.Option) (c *Cached[float64, any]) {
+func NewNumberCache(freq, ttl time.Duration, u units.MeasureUnit, opt ...units.Option) (c *Cached[float64, any]) {
 	c = &Cached[float64, any]{
-		Cache: units.NewCache[float64, any](0, nil, freq, ttl, units, opt...),
+		Cache: units.NewCache[float64, any](0, nil, freq, ttl, u, opt...),
 	}
 	c.StartKeeping()
 	return
